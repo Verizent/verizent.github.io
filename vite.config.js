b936fa66b4f2711d3c22542ed5ceb99373f7f437
Verizent <101114977+Verizent@users.noreply.github.com>
2025-10-16 09:52:16 +0800

    undo prev commit and fix email links

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginRadar({
-    analytics: {
-      id: process.env.VITE_GOOGLE_MEASUREMENT_ID,
-    },
-  })],
+  plugins: [react()],
   build: {
     outDir: 'docs'
   },
