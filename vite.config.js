2f3d424d0c3f94763fe52aac614f652bbc874687
Verizent <101114977+Verizent@users.noreply.github.com>
2025-10-16 09:41:16 +0800

    add analytics

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({
+    analytics: {
+      id: process.env.VITE_GOOGLE_MEASUREMENT_ID,
+    },
+  })],
   build: {
     outDir: 'docs'
   },
